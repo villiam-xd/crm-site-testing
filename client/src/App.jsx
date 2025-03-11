cbb6a9668dee1cdd5d42f2b9523f2de95d00298c
unknown <linus.lindroth.92@gmail.com>
2025-03-11 10:44:27 +0100

    changed name on 'App.jsx -> Router.jsx ' and 'main.jsx -> App.jsx'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router";
-import Layout from "./Layout.jsx";
-import Home from "./pages/Home.jsx";
-import Register from "./pages/Register.jsx";
-import Login from "./pages/Login.jsx";
-import EmployeeView from "./pages/admin/EmployeeView.jsx";
-import NewEmployee from "./pages/admin/NewEmployee.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Router from './Router.jsx'
+import { GlobalProvider } from './GlobalContext.jsx'
 
-export default function App() {
-  return <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />} >
-          <Route index element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin/employees" element={<EmployeeView />} />
-          <Route path="/admin/employees/new" element={<NewEmployee />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </>
-}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <GlobalProvider>
+      <Router />
+    </GlobalProvider>
+  </StrictMode>,
+)
---
+++ client/src/Router.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./Layout.jsx";
+import Home from "./pages/Home.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import EmployeeView from "./pages/admin/EmployeeView.jsx";
+import NewEmployee from "./pages/admin/NewEmployee.jsx";
+
+export default function Router() {
+  return <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/admin/employees" element={<EmployeeView />} />
+          <Route path="/admin/employees/new" element={<NewEmployee />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </>
+}
