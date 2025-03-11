e72b6dc3a06350550e270f915ec8ee373badb2c5
unknown <linus.lindroth.92@gmail.com>
2025-03-11 13:33:13 +0100

    added employee menu with an 'Edit information'-button and form

--- client/src/components/EmployeeCard.jsx
+++ client/src/components/EmployeeCard.jsx
@@ -1,9 +1,66 @@
+import { useState } from "react"
+
 export default function EmployeeCard({ employee }) {
+    const [hide, setHide] = useState(false)
+    const [editForm, setEditForm] = useState(false)
+
+    function toggleMenus() {
+        if (hide) {
+            setEditForm(false)
+        }
+        setHide(!hide)
+    }
+
+    async function updateEmployee(formdata) {
+        const response = await fetch(`/api/users/${employee.id}`, {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            credentials: "included",
+            body: JSON.stringify({
+                firstname: formdata("firstname"),
+                lastname: formdata("lastname"),
+                email: formdata("email"),
+                role: formdata("role"),
+            })
+        })
+
+        if (response.ok) {
+            alert("Good")
+        } else {
+            alert("Bad")
+        }
+    }
+
     return <div className="employeeCard">
-        <p>{employee.email}</p>
-        <p>{employee.username}</p>
-        <p>{employee.firstname}</p>
-        <p>{employee.lastname}</p>
-        <p>{employee.role}</p>
+        <div className={"employeeAttributes" + (hide ? " isActive" : "")} onClick={toggleMenus}>
+            <p>{employee.firstname}</p>
+            <p>{employee.lastname}</p>
+            <p>{employee.email}</p>
+            <p>{employee.role}</p>
+            <p>{employee.username}</p>
+        </div>
+        {
+            editForm ?
+                <form action={updateEmployee} className="editForm">
+                    <input type="text" name="firstname" placeholder="New Firstname" />
+                    <input type="text" name="lastname" placeholder="New Lastname" />
+                    <input type="email" name="email" placeholder="New Email" />
+                    <select defaultValue={employee.role}>
+                        <option value="USER">USER</option>
+                        <option value="ADMIN">ADMIN</option>
+                    </select>
+                    <button type="submit">Update Employee</button>
+                </form>
+                :
+                null
+        }
+        {
+            hide ?
+                <div className="employeeMenu">
+                    <button onClick={() => setEditForm(!editForm)}>Edit information</button>
+                </div>
+                :
+                null
+        }
     </div>
 }
--- client/src/components/EmployeeList.jsx
+++ client/src/components/EmployeeList.jsx
@@ -3,11 +3,11 @@ import EmployeeCard from "./EmployeeCard.jsx";
 export default function EmployeeList({ employeeList }) {
     return <div className="list">
         <div className="employeeListHeaders">
-            <p>Email</p>
-            <p>Username</p>
             <p>Firstname</p>
             <p>Lastname</p>
+            <p>Email</p>
             <p>ROLE</p>
+            <p>Username</p>
         </div>
         {employeeList.map(employee => <EmployeeCard key={employee.id} employee={employee} />)}
     </div>
