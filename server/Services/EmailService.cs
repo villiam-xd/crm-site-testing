5c080de47c52e99c10422b4090a0e31d39f8351f
unknown <linus.lindroth.92@gmail.com>
2025-03-19 07:35:05 +0100

    wip implemented Mailkit

---
+++ server/Services/EmailService.cs
@@ -0,0 +1,36 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using server.Config;
+
+namespace server.Services;
+
+public interface IEmailService
+{
+    Task SendEmailAsync(string to, string subject, string body);
+}
+
+public class EmailService
+{
+    private readonly EmailSettings _settings;
+
+    public EmailService(EmailSettings settings) => _settings = settings;
+
+    public async Task SendEmailAsync(string to, string subject, string body)
+    {
+        var email = new MimeMessage()
+            
+        {
+            From = { MailboxAddress.Parse(_settings.FromEmail) },
+            To = { MailboxAddress.Parse(to) },
+            Subject = subject,
+            Body = new TextPart("plain") { Text = body }
+        };
+
+        using var smtp = new SmtpClient();
+        await smtp.ConnectAsync(_settings.SmtpServer, _settings.SmtpPort, SecureSocketOptions.StartTls);
+        await smtp.AuthenticateAsync(_settings.FromEmail, _settings.Password);
+        await smtp.SendAsync(email);
+        await smtp.DisconnectAsync(true);
+    }
+}
